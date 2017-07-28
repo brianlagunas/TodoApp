303c1a61fe4fca5566d7173288dde180e3721fdb
Brian Lagunas <brian.lagunas@live.com>
2017-07-27 19:34:34 -0600

    added error handling in XF app

--- TodoApp/TodoApp/App.xaml.cs
+++ TodoApp/TodoApp/App.xaml.cs
@@ -5,29 +5,57 @@ using TodoApp.Services;
 using Microsoft.Azure.Mobile;
 using Microsoft.Azure.Mobile.Analytics;
 using Microsoft.Azure.Mobile.Crashes;
+using System;
+using System.Threading.Tasks;
 
 namespace TodoApp
 {
-    public partial class App : PrismApplication
-    {
-        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+	public partial class App : PrismApplication
+	{
+		public App(IPlatformInitializer initializer = null) : base(initializer) { }
 
-        protected override void OnInitialized()
-        {
-            InitializeComponent();
+		protected async override void OnInitialized()
+		{
+			InitializeComponent();
 
-            NavigationService.NavigateAsync("MainPage");
-        }
+			TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
 
-        protected override void RegisterTypes()
-        {
-            Container.RegisterTypeForNavigation<MainPage>();
-            Container.RegisterType<ITodoService, AzureTodoService>(new ContainerControlledLifetimeManager());
-        }
+			try
+			{
+				await NavigationService.NavigateAsync("MainPage");
+			}
+			catch (Exception ex)
+			{
+				ShowCrashPage(ex);
+			}
+		}
+
+		protected override void RegisterTypes()
+		{
+			Container.RegisterTypeForNavigation<MainPage>();
+			Container.RegisterType<ITodoService, AzureTodoService>(new ContainerControlledLifetimeManager());
+		}
 
 		protected override void OnStart()
 		{
 			MobileCenter.Start("android=41320330-983d-4d55-8020-56fb7bf8c907;", typeof(Analytics), typeof(Crashes));
+		}
+
+		void ShowCrashPage(Exception ex = null)
+		{
+			Xamarin.Forms.Device.BeginInvokeOnMainThread(() => this.MainPage = new CrashPage(ex?.Message));
+		}
+
+		void TaskScheduler_UnobservedTaskException(Object sender, UnobservedTaskExceptionEventArgs e)
+		{
+			if (!e.Observed)
+			{
+				// prevents the app domain from being torn down
+				e.SetObserved();
+
+				// show the crash page
+				ShowCrashPage(e.Exception.Flatten().GetBaseException());
+			}
 		}
 	}
 }
---
+++ TodoApp/TodoApp/Views/CrashPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TodoApp.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CrashPage : ContentPage
+	{
+		public String ExceptionMessage { get; } = String.Empty;
+
+		public CrashPage()
+		{
+			InitializeComponent();
+			this.BindingContext = this;
+		}
+
+		public CrashPage(String exceptionMessage)
+		{
+			InitializeComponent();
+			BindingContext = this;
+			ExceptionMessage = exceptionMessage;
+		}
+	}
+}
